5f17e2baf40e3ae71754c7de517f8e79444cfc3e
talhaamaan49 <144532978+talhaamaan49@users.noreply.github.com>
2023-10-01 19:21:27 +0530

    Update config.sql

--- config.sql
+++ config.sql
@@ -1,3 +1,2 @@
 create user 'webdatabase'@'%' identified by 'webdatabase';
-grant all privileges on *.* to 'webdatabase'@'%' with grant option;
-flush privileges;
+grant all privileges on *.* to 'webdatabase'@'%';
